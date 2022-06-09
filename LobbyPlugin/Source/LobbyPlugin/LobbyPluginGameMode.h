9b84a20ff803f6b2608f880caac8a4861c74f878
Artem <34547317+Zenslash@users.noreply.github.com>
2022-06-09 11:23:07 +0300

    Steam setup

---
+++ LobbyPlugin/Source/LobbyPlugin/LobbyPluginGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LobbyPluginGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ALobbyPluginGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ALobbyPluginGameMode();
+};
+
+
+
