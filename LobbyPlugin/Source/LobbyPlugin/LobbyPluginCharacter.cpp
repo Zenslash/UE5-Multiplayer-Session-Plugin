880beb5b3de3caaa720fe4dc91572b3c8414fbf9
Artem <34547317+Zenslash@users.noreply.github.com>
2022-06-09 16:51:57 +0300

    Connect to Steam subsystem

--- LobbyPlugin/Source/LobbyPlugin/LobbyPluginCharacter.cpp
+++ LobbyPlugin/Source/LobbyPlugin/LobbyPluginCharacter.cpp
@@ -7,6 +7,8 @@
 #include "GameFramework/CharacterMovementComponent.h"
 #include "GameFramework/Controller.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "OnlineSubsystem.h"
+#include "Interfaces/OnlineSessionInterface.h"
 
 //////////////////////////////////////////////////////////////////////////
 // ALobbyPluginCharacter
@@ -49,6 +51,23 @@ ALobbyPluginCharacter::ALobbyPluginCharacter()
 
 	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
 	// are set in the derived blueprint asset named ThirdPersonCharacter (to avoid direct content references in C++)
+
+	//Online Subsystem
+	IOnlineSubsystem* OnlineSubsystem = IOnlineSubsystem::Get();
+	if(OnlineSubsystem)
+	{
+		OnlineSessionInterface = OnlineSubsystem->GetSessionInterface();
+
+		if(GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(-1,
+				15.f,
+				FColor::Blue,
+				FString::Printf(TEXT("Found subsystem %s"),
+					*OnlineSubsystem->GetSubsystemName().ToString())
+			);
+		}
+	}
 }
 
 //////////////////////////////////////////////////////////////////////////
