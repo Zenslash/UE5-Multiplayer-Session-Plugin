04e7c6fe7acf4ccc7c7863b17a47299395e5aab5
Artem <34547317+Zenslash@users.noreply.github.com>
2022-06-13 11:40:17 +0300

    Create Menu class

---
+++ LobbyPlugin/Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/MenuWidget.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MenuWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERSESSIONS_API UMenuWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	UFUNCTION(BlueprintCallable)
+	void MenuSetup();
+
+};
